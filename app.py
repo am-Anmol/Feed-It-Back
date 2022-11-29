2d5374e7862a1cf1740b17738f28726f08b45a41
Anmol Gupta <guptaanmol248001@gmail.com>
2022-11-29 11:15:49 +0530

    Added login and register forms

--- app.py
+++ app.py
@@ -10,4 +10,16 @@ def Index():
 def about():
     return render_template('aboutus.html')
 
+@app.route("/donorRegister")
+def dregister():
+    return render_template('donorRegister.html')
+
+@app.route("/volunteerRegister")
+def vregister():
+    return render_template('volunteerRegister.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
 app.run(debug=True)
