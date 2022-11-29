3f843bfaab9ec2f6a2b51a53adb941f673b7c620
Anmol Gupta <guptaanmol248001@gmail.com>
2022-11-29 10:22:25 +0530

    Added about us

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app=Flask(__name__,static_url_path='/static')
 def Index():
     return render_template('index.html')
 
+@app.route("/aboutus")
+def about():
+    return render_template('aboutus.html')
 
 app.run(debug=True)
